81414cd14c1cd38dab5cf087f4fc5a54c3008aec
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-01 15:45:51 -0500

    Update tests for Generate -> RandomDraw rename

--- tests/operations/unary/RandomDraw.cpp
+++ tests/operations/unary/RandomDraw.cpp
@@ -1,11 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "Catch/single_include/catch2/catch.hpp"
 
-#include "sgpl/operations/unary/Generate.hpp"
+#include "sgpl/operations/unary/RandomDraw.hpp"
 
-TEST_CASE("Test Generate") {
+TEST_CASE("Test RandomDraw") {
 
   // TODO flesh out stub test
-  sgpl::Generate{};
+  sgpl::RandomDraw{};
 
 }
