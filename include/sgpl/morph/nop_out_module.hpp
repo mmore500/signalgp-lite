ae96ae64676eed55ec3df836e2dfeae3b5e94b08
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-24 05:50:13 -0400

    Fix bug where sequential module nops interfered

--- include/sgpl/morph/nop_out_module.hpp
+++ include/sgpl/morph/nop_out_module.hpp
@@ -4,8 +4,10 @@
 
 #include <iterator>
 
+#include "../../../third-party/Empirical/include/emp/base/assert.hpp"
 #include "../../../third-party/Empirical/include/emp/base/vector.hpp"
 
+#include "../introspection/count_modules.hpp"
 #include "../introspection/get_module_length.hpp"
 #include "../introspection/get_module_pos.hpp"
 #include "../program/GlobalAnchorIterator.hpp"
@@ -20,16 +22,16 @@ sgpl::Program<Spec> nop_out_module(
   sgpl::Program<Spec> program, const size_t module_idx
 ) {
 
+  emp_assert( module_idx < sgpl::count_modules<Spec>( program ) );
+
   const size_t module_pos = sgpl::get_module_pos( program, module_idx );
   const size_t module_length = sgpl::get_module_length( program, module_idx );
 
-  emp::vector< char > should_nop;
+  emp::vector< char > should_nop( module_pos, false );
 
-  const emp::vector< char > before(module_pos, false);
-  const emp::vector< char > module(module_length, true);
-
-  should_nop.insert(std::end(should_nop), std::begin(before), std::end(before));
+  const emp::vector< char > module( module_length, true );
   should_nop.insert(std::end(should_nop), std::begin(module), std::end(module));
+
   should_nop.resize( program.size(), false );
 
   return sgpl::nop_out_instructions<Spec>( program, should_nop );
