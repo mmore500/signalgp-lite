2f331d3d179965d5163b09d0298eb785551a23a5
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-21 20:06:02 -0400

    Update compiler flags in Makefile recipes

--- tests/convert_for_tests.sh
+++ tests/convert_for_tests.sh
@@ -8,7 +8,7 @@ cp -r ${TO_ROOT}/include ${TO_ROOT}/coverage_include
 for filename in $(find ${TO_ROOT}/coverage_include -name "*.hpp")
 do
     echo $filename
-    ${TO_ROOT}/third-party/force-cover/force_cover $filename -- -I. -I${TO_ROOT}/coverage_include --language c++ -std=c++17 -DEMP_TRACK_MEM -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/x86_64-linux-gnu/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8/backward -I/usr/include/clang/7/include -I/usr/local/include -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/include -I/usr/include/x86_64-linux-gnu -I/usr/include -I${TO_ROOT}/third-party/cereal/include/ | xargs -0 echo > $filename.temp
+    ${TO_ROOT}/third-party/force-cover/force_cover $filename -- -I. -I${TO_ROOT}/coverage_include --language c++ -std=c++17 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/x86_64-linux-gnu/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8/backward -I/usr/include/clang/7/include -I/usr/local/include -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/include -I/usr/include/x86_64-linux-gnu -I/usr/include -I${TO_ROOT}/third-party/cereal/include/ | xargs -0 echo > $filename.temp
     mv $filename.temp $filename
 
 done
