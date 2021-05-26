f7626402aa92c8510fdd24ea17da22abc9c5b889
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-05-26 12:13:00 -0400

    Use ToggleRegulationOpLibrary instead

--- demos/contextual-signal/contextual-signal.cpp
+++ demos/contextual-signal/contextual-signal.cpp
@@ -12,13 +12,13 @@
 #include "../include/sgpl/algorithm/execute_cpu.hpp"
 #include "../include/sgpl/hardware/Cpu.hpp"
 #include "../include/sgpl/library/OpLibraryCoupler.hpp"
-#include "../include/sgpl/library/prefab/CompleteOpLibrary.hpp"
 #include "../include/sgpl/program/Program.hpp"
 #include "../include/sgpl/spec/Spec.hpp"
 #include "../include/sgpl/utility/ThreadLocalRandom.hpp"
 
 #include "config.hpp"
 
+#include "ToggleRegulationOpLibrary.hpp"
 
 const size_t PopulationSize = 1000;
 
@@ -62,7 +62,7 @@ struct SetState {
     }
 };
 
-using library_t = sgpl::OpLibraryCoupler<sgpl::CompleteOpLibrary, SetState>;
+using library_t = sgpl::OpLibraryCoupler<sgpl::ToggleRegulationOpLibrary, SetState>;
 
 // TODO: override spec and change line 34 to ratio<1,2>
 using spec_t = sgpl::Spec<library_t, Peripheral>;
