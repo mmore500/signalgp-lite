cfbd17ae63f38d817093167e26a80ebb917b8077
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-09 18:20:45 -0400

    Add actions unit tests

--- tests/operations/actions/TerminateIf.cpp
+++ tests/operations/actions/TerminateIf.cpp
@@ -3,9 +3,42 @@
 
 #include "sgpl/operations/actions/TerminateIf.hpp"
 
+
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
+
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+// define libray and spec
+using library_t = sgpl::OpLibrary<sgpl::TerminateIf>;
+
+using spec_t = sgpl::Spec<library_t>;
+
+// create peripheral
+spec_t::peripheral_t peripheral;
+
 TEST_CASE("Test TerminateIf") {
 
-  // TODO flesh out stub test
-  sgpl::TerminateIf{};
+  sgpl::Program<spec_t> program{1};
 
+  sgpl::Core<spec_t> core;
+
+  // set register to true
+  core.registers[0] = true;
+
+  // tell terminateif to check inside the 0th register
+  program[0].args[0] = 0;
+
+  // check initial state
+  REQUIRE(!core.HasTerminated());
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // check final state
+  REQUIRE(core.HasTerminated());
 }
