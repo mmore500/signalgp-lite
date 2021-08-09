f4149a5434a91f00c5622b893d24840072722202
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-09 18:23:45 -0400

    Add unary unit tests

--- tests/operations/unary/Increment.cpp
+++ tests/operations/unary/Increment.cpp
@@ -3,9 +3,49 @@
 
 #include "sgpl/operations/unary/Increment.hpp"
 
+
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
+
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+// define libray and spec
+using library_t = sgpl::OpLibrary<sgpl::Increment>;
+
+using spec_t = sgpl::Spec<library_t>;
+
+// create peripheral
+spec_t::peripheral_t peripheral;
+
 TEST_CASE("Test Increment") {
 
-  // TODO flesh out stub test
-  sgpl::Increment{};
+  sgpl::Program<spec_t> program{1};
+
+  sgpl::Core<spec_t> core;
+
+  // set up false check
+
+  // set up values to operate on in register
+  core.registers[0] = 99;
+
+  // set up what registers to operate on
+  program[0].args[0] = 0;
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{99, 0, 0, 0, 0, 0, 0, 0}
+  ));
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // check final state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{100, 0, 0, 0, 0, 0, 0, 0}
+  ));
 
 }
--- tests/operations/unary/RandomBool.cpp
+++ tests/operations/unary/RandomBool.cpp
@@ -3,9 +3,62 @@
 
 #include "sgpl/operations/unary/RandomBool.hpp"
 
-TEST_CASE("Test RandomBool") {
+#include "sgpl/utility/ThreadLocalRandom.hpp"
 
-  // TODO flesh out stub test
-  sgpl::RandomBool{};
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
 
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+using spec_t = sgpl::Spec<sgpl::OpLibrary<sgpl::RandomBool>>;
+
+auto map_to_unit = [](const typename spec_t::tag_t& tag) -> double {
+
+  constexpr double max_double = spec_t::tag_t::MaxDouble();
+
+  return tag.GetDouble() / max_double;
+
+};
+
+TEMPLATE_TEST_CASE_SIG("Test RandomBool", "[Nop]",
+  ((int K), K), 1, 2, 3, 4, 5
+) {
+  // create peripheral
+  typename spec_t::peripheral_t peripheral;
+
+  sgpl::Program<spec_t> program{1};
+
+  sgpl::Core<spec_t> core;
+
+  // set up what register to operate on
+  program[0].args[0] = 0;
+
+  // initialize rand
+  emp::Random rand(1);
+
+  // initialize tlrand
+  sgpl::tlrand.Reseed(1);
+
+  // check that internal RNG is what we expect
+  REQUIRE(sgpl::tlrand.Get().GetUInt() == rand.GetUInt());
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{0, 0, 0, 0, 0, 0, 0, 0}
+  ));
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // get p
+  const double p = map_to_unit(program[0].tag);
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{rand.P(p), 0, 0, 0, 0, 0, 0, 0}
+  ));
 }
