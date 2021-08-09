55aa7a01f766e6b95a741738ee660d5233d8233c
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-09 18:20:45 -0400

    Add actions unit tests

--- tests/operations/actions/Nop.cpp
+++ tests/operations/actions/Nop.cpp
@@ -3,9 +3,44 @@
 
 #include "sgpl/operations/actions/Nop.hpp"
 
-TEST_CASE("Test Nop") {
+#include "sgpl/utility/ThreadLocalRandom.hpp"
 
-  // TODO flesh out stub test
-  sgpl::Nop{};
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
 
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+TEMPLATE_TEST_CASE_SIG("Test Nop", "[Nop]",
+  ((int K), K), 1, 2, 3, 4, 5
+) {
+  // define spec
+  using spec_t = sgpl::Spec<sgpl::OpLibrary<sgpl::Nop<K>>>;
+
+  // create peripheral
+  typename spec_t::peripheral_t peripheral;
+
+  emp::Random rand(1);
+
+  sgpl::Program<spec_t> program{1};
+
+  sgpl::Core<spec_t> core;
+
+  // initialize tlrand
+  sgpl::tlrand.Reseed(1);
+
+  // check that internal RNG is what we expect
+  REQUIRE(sgpl::tlrand.Get().GetUInt() == rand.GetUInt());
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // advance our rand K times
+  for (int i{}; i < K; ++i) rand.StepEngine();
+
+  // check that internal RNG has been advanced as we expect
+  REQUIRE(sgpl::tlrand.Get().GetUInt() == rand.GetUInt());
 }
