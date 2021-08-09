0257bd78eff5eb12e1270c69fd59534646808461
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-09 18:23:45 -0400

    Add unary unit tests

--- tests/operations/unary/RandomDraw.cpp
+++ tests/operations/unary/RandomDraw.cpp
@@ -3,9 +3,76 @@
 
 #include "sgpl/operations/unary/RandomDraw.hpp"
 
+#include "sgpl/utility/ThreadLocalRandom.hpp"
+
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
+
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+using spec_t = sgpl::Spec<sgpl::OpLibrary<sgpl::RandomBool>>;
+
+
+auto map_between_plusminus_one = [](const typename spec_t::tag_t& tag) {
+  constexpr double max = 1.0;
+  constexpr double min = -1.0;
+  constexpr double max_double = spec_t::tag_t::MaxDouble();
+
+  return  (tag.GetDouble() / max_double) * (max - min) + min;
+};
+
+auto is_odd = [](const typename spec_t::tag_t& tag) {
+  return tag.Get(0);
+};
+
+auto map_up = [](const double plusminus_unit_val) {
+  return 1.0 / (
+    plusminus_unit_val * plusminus_unit_val * plusminus_unit_val
+  );
+};
+
+auto map_draw = [](emp::Random& rand) -> double {
+  const typename spec_t::tag_t tag( rand );
+  return is_odd( tag )
+    ? map_up( map_between_plusminus_one(tag) )
+    : map_between_plusminus_one( tag )
+  ;
+};
+
 TEST_CASE("Test RandomDraw") {
+  // create peripheral
+  typename spec_t::peripheral_t peripheral;
 
-  // TODO flesh out stub test
-  sgpl::RandomDraw{};
+  sgpl::Program<spec_t> program{1};
 
+  sgpl::Core<spec_t> core;
+
+  // set up what register to operate on
+  program[0].args[0] = 0;
+
+  // initialize rand
+  emp::Random rand(1);
+
+  // initialize tlrand
+  sgpl::tlrand.Reseed(1);
+
+  // check that internal RNG is what we expect
+  REQUIRE(sgpl::tlrand.Get().GetUInt() == rand.GetUInt());
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{0, 0, 0, 0, 0, 0, 0, 0}
+  ));
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{map_draw(rand), 0, 0, 0, 0, 0, 0, 0}
+  ));
 }
