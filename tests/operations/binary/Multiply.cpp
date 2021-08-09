a407283b4fecc9f81f935b91ec139f1afc9d9253
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-09 18:22:01 -0400

    Add binary unit tests

--- tests/operations/binary/Multiply.cpp
+++ tests/operations/binary/Multiply.cpp
@@ -3,9 +3,50 @@
 
 #include "sgpl/operations/binary/Multiply.hpp"
 
+#include "sgpl/hardware/Core.hpp"
+#include "sgpl/program/Program.hpp"
+
+#include "sgpl/algorithm/execute_core.hpp"
+
+#include "sgpl/spec/Spec.hpp"
+
+#include "sgpl/utility/EmptyType.hpp"
+
+// define libray and spec
+using library_t = sgpl::OpLibrary<sgpl::Multiply>;
+
+using spec_t = sgpl::Spec<library_t>;
+
+// create peripheral
+spec_t::peripheral_t peripheral;
+
 TEST_CASE("Test Multiply") {
 
-  // TODO flesh out stub test
-  sgpl::Multiply{};
+  sgpl::Program<spec_t> program{1};
 
+  sgpl::Core<spec_t> core;
+
+  // set up initial state
+
+  // set up values to operate on in register
+  core.registers[0] = 99;
+  core.registers[1] = 7;
+
+  // set up what registers to operate on
+  program[0].args[0] = 2;
+  program[0].args[1] = 0;
+  program[0].args[2] = 1;
+
+  // check initial state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{99, 7, 0, 0, 0, 0, 0, 0}
+  ));
+
+  // execute single instruction
+  sgpl::advance_core(core, program, peripheral);
+
+  // check final state
+  REQUIRE_THAT(core.registers, Catch::Matchers::Equals(
+    emp::array<float, 8>{99, 7, 693, 0, 0, 0, 0, 0}
+  ));
 }
