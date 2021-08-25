d5a64160a4364a6b784cb69ee5405ffc478166ac
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-25 17:19:06 -0400

    Add RandomFill

---
+++ tests/operations/bitwise/RandomFill.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "sgpl/operations/bitwise/RandomFill.hpp"
+
+TEST_CASE("Test RandomFill") {
+
+  // TODO flesh out stub test
+  sgpl::RandomFill{};
+
+}
