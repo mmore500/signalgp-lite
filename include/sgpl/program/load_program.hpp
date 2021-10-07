fae0e38b79d820b22bb4e16658fbe0389925fbb1
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-10-07 14:17:43 -0400

    Move LoadProgram into SGPL

---
+++ include/sgpl/program/load_program.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef SGPL_PROGRAM_LOAD_PROGRAM_HPP_INCLUDE
+#define SGPL_PROGRAM_LOAD_PROGRAM_HPP_INCLUDE
+
+#include <fstream>
+#include <string>
+
+#include "../../../third-party/cereal/include/cereal/archives/binary.hpp"
+#include "../../../third-party/cereal/include/cereal/archives/json.hpp"
+#include "../../../third-party/cereal/include/cereal/cereal.hpp"
+#include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
+#include "../../../third-party/Empirical/include/emp/base/error.hpp"
+
+#include "Program.hpp"
+
+namespace sgpl {
+
+template <typename Spec>
+sgpl::Program<Spec> load_program(const std::filesystem::path& path) {
+    sgpl::Program<Spec> program;
+
+    std::ifstream is(path);
+
+    if (path.extension() == ".json") {
+        cereal::JSONInputArchive archive( is );
+    } else if (path.extension() == ".bin") {
+        cereal::BinaryInputArchive archive( is );
+    } else {
+        emp_error("Unknown extension.", path, path.extension());
+    }
+
+    archive( program );
+
+    return program;
+}
+
+#endif // #ifndef SGPL_PROGRAM_LOAD_PROGRAM_HPP_INCLUDE
