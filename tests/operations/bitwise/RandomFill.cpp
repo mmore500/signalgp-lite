8a85571d509037e87d7bb51be06f1027686018e1
rodsan0 <rodsan0@users.noreply.github.com>
2021-10-12 16:51:04 -0400

    Change 'anded' and 'ored' to 'and_ed' and 'or_ed'

--- tests/operations/bitwise/RandomFill.cpp
+++ tests/operations/bitwise/RandomFill.cpp
@@ -21,8 +21,8 @@ TEST_CASE("Test RandomFill") {
   // initialize tlrand
   sgpl::tlrand.Reseed(1);
 
-  int anded{};
-  int ored{};
+  int and_ed{};
+  int or_ed{};
 
   for (size_t i{}; i < replicates; i++) {
     // create and initialize cpu
@@ -36,14 +36,14 @@ TEST_CASE("Test RandomFill") {
     // execute instruction
     sgpl::execute_cpu(1, cpu, program);
 
-    anded &= std::bit_cast<int>(cpu.GetActiveCore().registers[0]);
-    ored |= std::bit_cast<int>(cpu.GetActiveCore().registers[0]);
+    and_ed &= std::bit_cast<int>(cpu.GetActiveCore().registers[0]);
+    or_ed |= std::bit_cast<int>(cpu.GetActiveCore().registers[0]);
   }
 
   // check that every bit as been a 1 and a 0 at least once
   // we do this by ANDing and ORing every result
-  // at the end, anded should just be zeros, and ored should just be ones
+  // at the end, and_ed should just be zeros, and or_ed should just be ones
   // and that the max and min bounds are proper
-  REQUIRE(anded == 0);
-  REQUIRE(ored == -1); // in two's compliment, -1 is represented by setting all bits to 1
+  REQUIRE(and_ed == 0);
+  REQUIRE(or_ed == -1); // in two's compliment, -1 is represented by setting all bits to 1
 }
