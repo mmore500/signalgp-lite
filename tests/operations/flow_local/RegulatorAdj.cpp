40f368c988b15ebf265f2e9b83bb441ac95f97d8
rodsan0 <rodsan0@users.noreply.github.com>
2021-10-12 16:50:23 -0400

    Change decay to adjust inside RegulatorAdj

--- tests/operations/flow_local/RegulatorAdj.cpp
+++ tests/operations/flow_local/RegulatorAdj.cpp
@@ -39,7 +39,7 @@ TEST_CASE("Test RegulatorAdj") {
   // execute RegulatorSet
   sgpl::advance_core(core, program);
 
-  // set register to a big number (amount to decay by)
+  // set register to a big number (amount to adjust by)
   core.registers[1] = 100000;
 
   REQUIRE(core.registers == emp::array<float, 4>{99, 100000, 0, 0});
