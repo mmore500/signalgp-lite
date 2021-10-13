d33dbd3e7a50f647dfa4200457f311ed7f97b158
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-10-12 20:02:19 -0400

    Explain template variable

--- tests/operations/actions/Nop.cpp
+++ tests/operations/actions/Nop.cpp
@@ -18,6 +18,7 @@ template<int K> using program_t = sgpl::Program<spec_t<K>>;
  * This is a templated test case.
  * This means that a unique test case will be created
  * for each of the ints in the title, with K substituted in.
+ * K represents the number of RNG touches applied by the Nop instruction.
 */
 TEMPLATE_TEST_CASE_SIG("Test Nop", "[Nop]",
   ((int K), K), 1, 2, 3, 4, 5
