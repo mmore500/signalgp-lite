d2ef0b650f58fff77a8dba2198b235fc6f6f76bd
rodsan0 <rodsan0@users.noreply.github.com>
2021-12-28 13:49:07 -0500

    Disable mutation occurence rate
    *based on experimental results

--- demos/boolean-calculator/include/bc/config.hpp
+++ demos/boolean-calculator/include/bc/config.hpp
@@ -13,7 +13,7 @@ EMP_BUILD_CONFIG(
   VALUE(SGPL_MINOR_SEQUENCE_MUTATION_BOUND, size_t, 8, "TODO"),
   VALUE(SGPL_POINT_MUTATION_RATE, float, 0.0002, "What fraction of bits should be scrambled?"),
   VALUE(SGPL_SEQUENCE_DEFECT_RATE, float, 0.001, "How often should sloppy copy defect occur?"),
-  VALUE(SGPL_MUTATION_OCCURENCE_RATE, float, 0.5, "How often should mutations happen?"),
+  VALUE(SGPL_MUTATION_OCCURENCE_RATE, float, 1.0, "How often should mutations happen?"),
   VALUE(ENVIROMENT_STATES, size_t, 4, "TODO"),
   VALUE(NUMBER_RESPONSES, size_t, 4, "TODO"),
   VALUE(UPDATES, size_t, 100, "TODO"),
