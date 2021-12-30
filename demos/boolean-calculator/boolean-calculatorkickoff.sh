a5329b51b7784ca179f32120671a140f10574f3d
rodsan0 <rodsan0@users.noreply.github.com>
2021-12-30 17:28:39 -0500

    Update SLURM scripts

--- demos/boolean-calculator/boolean-calculatorkickoff.sh
+++ demos/boolean-calculator/boolean-calculatorkickoff.sh
@@ -2,6 +2,8 @@
 
 TIME=$(date '+%F_%H-%M-%S')
 
+mkdir "~/slurmscripts/${TIME}"
+
 mkdir "./${TIME}"
 cd "./${TIME}"
 
@@ -9,9 +11,8 @@ cd "./${TIME}"
 for point_rate in 0.0002 0.00006 0.00002; do
     for sequence_rate in 0.001 0.0003 0.0001; do
         for replicate in $(seq 1 10); do
-            JOB_SCRIPT="boolean_calculator+point_rate=${point_rate}+sequence_rate=${sequence_rate}+replicate=${replicate}+ext=.sh"
-
-            echo $JOB_SCRIPT
+            # JOB_SCRIPT="boolean_calculator+point_rate=${point_rate}+sequence_rate=${sequence_rate}+replicate=${replicate}+ext=.sh"
+            JOB_SCRIPT="\$(mktemp)"
 
             j2 --format=yaml -o "${JOB_SCRIPT}" "../boolean-calculator.slurm.sh.jinja" << J2_HEREDOC_EOF
 point_rate: "${point_rate}"
@@ -24,7 +25,9 @@ J2_HEREDOC_EOF
 
             chmod +x ${JOB_SCRIPT}
 
-            # sbatch ${JOB_SCRIPT}
+            JOB_ID=$(sbatch --parsable ${JOB_SCRIPT})
+
+            cp ${JOB_SCRIPT} "~/slurmscripts/${TIME}/${JOB_ID}.sh"
         done
     done
 done
