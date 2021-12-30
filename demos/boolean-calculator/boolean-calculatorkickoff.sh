672434ade3eafe28447e97af4c49c237d1de57ab
rodsan0 <rodsan0@users.noreply.github.com>
2021-12-30 15:47:17 -0500

    Add kickoff and jinja scripts

---
+++ demos/boolean-calculator/boolean-calculatorkickoff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TIME=$(date '+%F_%H:%M:%S')
+
+mkdir "~/${TIME}"
+cd "~/${TIME}"
+
+
+for point_rate in 0.0002 0.00006 0.00002; do
+    for sequence_rate in 0.001 0.0003 0.0001; do
+        for replicate in $(seq 1 10); do
+            JOB_SCRIPT="boolean_calculator+point_rate=${point_rate}+sequence_rate=${sequence_rate}+replicate=${replicate}+ext=.sh"
+
+            echo $JOB_SCRIPT
+
+            j2 --format=yaml -o "${JOB_SCRIPT}" "boolean-calculator.slurm.sh.jinja" << J2_HEREDOC_EOF
+point_rate: "${point_rate}"
+sequence_rate: "${sequence_rate}"
+replicate: "${replicate}"
+time: "${TIME}"
+updates: "10000"
+J2_HEREDOC_EOF
+
+            chmod +x ${JOB_SCRIPT}
+
+
+        done
+    done
+done
