589a830c825d95b5ba0a0ce09e3ec54e7436f689
rodsan0 <rodsan0@users.noreply.github.com>
2021-12-30 17:40:30 -0500

    Fix mktemp call

--- demos/boolean-calculator/boolean-calculatorkickoff.sh
+++ demos/boolean-calculator/boolean-calculatorkickoff.sh
@@ -12,7 +12,7 @@ for point_rate in 0.0002 0.00006 0.00002; do
     for sequence_rate in 0.001 0.0003 0.0001; do
         for replicate in $(seq 1 10); do
             # JOB_SCRIPT="boolean_calculator+point_rate=${point_rate}+sequence_rate=${sequence_rate}+replicate=${replicate}+ext=.sh"
-            JOB_SCRIPT="\$(mktemp)"
+            JOB_SCRIPT=$(mktemp)
 
             j2 --format=yaml -o "${JOB_SCRIPT}" "../boolean-calculator.slurm.sh.jinja" << J2_HEREDOC_EOF
 point_rate: "${point_rate}"
