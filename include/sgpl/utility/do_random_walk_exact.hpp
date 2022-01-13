70be00173772fceb2ae19950332b279d8fa3e16f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-12 19:14:48 -0500

    Implement random walk helpers

---
+++ include/sgpl/utility/do_random_walk_exact.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef SGPL_UTILITY_DO_RANDOM_WALK_EXACT_HPP_INCLUDE
+#define SGPL_UTILITY_DO_RANDOM_WALK_EXACT_HPP_INCLUDE
+
+#include <cmath>
+
+#include "random_sign.hpp"
+
+namespace sgpl {
+
+// random walk with n steps on a one dimensional lattice
+int do_random_walk_exact(const int num_steps) {
+
+  emp_assert( num_steps >= 0, num_steps );
+
+  int res{};
+  for (int i{}; i < num_steps; ++i) res += sgpl::random_sign();
+
+  return res;
+
+}
+
+} // namespace sgpl
+
+#endif // #ifndef SGPL_UTILITY_DO_RANDOM_WALK_EXACT_HPP_INCLUDE
