4052f6769b7ba4e5fd01a1d259d56fa0bb6238ec
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-12 21:27:07 -0500

    Fix indentation

--- include/sgpl/algorithm/module_indel_copy.hpp
+++ include/sgpl/algorithm/module_indel_copy.hpp
@@ -19,70 +19,70 @@
 
 namespace sgpl {
 
-  struct DefaultRangeCopier {
+struct DefaultRangeCopier {
 
-    // return additional indels
-    template<typename InputIt, typename OutputIt>
-    size_t copy(InputIt first, InputIt last, OutputIt out) const {
-      std::copy(first, last, out);
-      return 0;
+  // return additional indels
+  template<typename InputIt, typename OutputIt>
+  size_t copy(InputIt first, InputIt last, OutputIt out) const {
+    std::copy(first, last, out);
+    return 0;
+  }
+
+};
+
+/*
+ * @note if output program length should be limited,
+ * use sgpl::CappedOutputIterator
+ */
+template <
+  typename ModuleIt,
+  typename OutputIt,
+  typename RangeCopier=DefaultRangeCopier
+>
+size_t module_indel_copy(
+  ModuleIt program_begin, ModuleIt program_end,
+  OutputIt out_iter,
+  const float p_defect, const float p_defect_is_insertion=0.5f,
+  const RangeCopier& range_copier=RangeCopier{}
+) {
+
+  size_t cumulative_insertion_deletion{};
+
+  // aft (front) and stern (back) like on a boat
+  auto stern_it = program_begin;
+  auto aft_it = program_begin;
+  if (program_begin != program_end) do {
+
+    stern_it = std::exchange( aft_it, std::next(aft_it) );
+
+    const auto cur_module_begin = stern_it.begin();
+    const auto cur_module_end = aft_it.begin();
+    const size_t cur_module_length = std::distance(
+      cur_module_begin,
+      cur_module_end
+    );
+
+    // draw number of module copies to copy into result
+    const bool do_indel = sgpl::tlrand.Get().P( p_defect );
+    const size_t num_module_copies = (
+      1 + do_indel * sgpl::random_sign( p_defect_is_insertion )
+    );
+
+    if (do_indel) cumulative_insertion_deletion += cur_module_length;
+
+    // copy module into result n times
+    for (size_t i{}; i < num_module_copies; ++i) {
+      cumulative_insertion_deletion += range_copier.copy(
+        cur_module_begin, cur_module_end,
+        out_iter
+      );
     }
 
-  };
+  } while ( aft_it != program_end );
 
-  /*
-   * @note if output program length should be limited,
-   * use sgpl::CappedOutputIterator
-   */
-  template <
-    typename ModuleIt,
-    typename OutputIt,
-    typename RangeCopier=DefaultRangeCopier
-  >
-  size_t module_indel_copy(
-    ModuleIt program_begin, ModuleIt program_end,
-    OutputIt out_iter,
-    const float p_defect, const float p_defect_is_insertion=0.5f,
-    const RangeCopier& range_copier=RangeCopier{}
-  ) {
+  return cumulative_insertion_deletion;
 
-    size_t cumulative_insertion_deletion{};
-
-    // aft (front) and stern (back) like on a boat
-    auto stern_it = program_begin;
-    auto aft_it = program_begin;
-    if (program_begin != program_end) do {
-
-      stern_it = std::exchange( aft_it, std::next(aft_it) );
-
-      const auto cur_module_begin = stern_it.begin();
-      const auto cur_module_end = aft_it.begin();
-      const size_t cur_module_length = std::distance(
-        cur_module_begin,
-        cur_module_end
-      );
-
-      // draw number of module copies to copy into result
-      const bool do_indel = sgpl::tlrand.Get().P( p_defect );
-      const size_t num_module_copies = (
-        1 + do_indel * sgpl::random_sign( p_defect_is_insertion )
-      );
-
-      if (do_indel) cumulative_insertion_deletion += cur_module_length;
-
-      // copy module into result n times
-      for (size_t i{}; i < num_module_copies; ++i) {
-        cumulative_insertion_deletion += range_copier.copy(
-          cur_module_begin, cur_module_end,
-          out_iter
-        );
-      }
-
-    } while ( aft_it != program_end );
-
-    return cumulative_insertion_deletion;
-
-  }
+}
 
 } // namespace sgpl
 
