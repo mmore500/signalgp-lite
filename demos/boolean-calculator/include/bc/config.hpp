d8602f051312f906389d0dd977cfeb8a83f26085
rodsan0 <rodsan0@users.noreply.github.com>
2022-02-10 15:19:29 -0500

    Add updated config params

--- demos/boolean-calculator/include/bc/config.hpp
+++ demos/boolean-calculator/include/bc/config.hpp
@@ -8,12 +8,18 @@ namespace bc {
 
 EMP_BUILD_CONFIG(
   Config,
-  VALUE(SGPL_PROGRAM_SIZE_CAP, size_t, 4096, "What size should programs be capped at?"),
 
+  GROUP(MUTATION, "MUTATION"),
+  VALUE(SGPL_PROGRAM_SIZE_CAP,
+    size_t, 4096,
+    "What size should programs be capped at?"
+  ),
   VALUE(SGPL_POINTMUTATE_BITFLIP_RATE,
     float, 0.0002f,
     "What fraction of bits should be flipped?"
   ),
+
+  GROUP(INDEL_MUTATION, "INDEL MUTATION"),
   VALUE(SGPL_SEQMUTATE_INST_INDEL_RATE,
     float, 0.001f,
     "How often should instruction indel mutations occur?"
@@ -41,6 +47,41 @@ EMP_BUILD_CONFIG(
   VALUE(REPLICATE,
     size_t, 0,
     "Replicate number (dummy value)"
+  ),
+
+
+  GROUP(TRANSPOSITION_MUTATION, "TRANSPOSITION MUTATION"),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_RATE,
+    float, 0.0001f,
+    "How often per instruction should transpose events arise?"
+  ),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_INVERSION_RATE,
+    float, 0.02f,
+    "How often should transposed windows be inverted?"
+  ),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_WINDOW_SIZE_PARETO_SHAPE_PARAM,
+    float, 0.5f,
+    "Shape parameter (\"alpha\") for distribution of transposed window sizes"
+    " when drawn from pareto distribution. "
+    "Constrained > 0."
+  ),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_WINDOW_SIZE_PARETO_SCALE_PARAM,
+    float, 2.0f,
+    "Scale parameter (\"lambda\") for distribution of transposed window sizes"
+    " when drawn from pareto distribution. "
+    "Constrained > 0."
+  ),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_WINDOW_DISP_PARETO_SHAPE_PARAM,
+    float, 0.5f,
+    "Scale param (\"alpha\") for distn of window transposition displacements"
+    " when drawn from pareto distribution. "
+    "Constrained > 0."
+  ),
+  VALUE(SGPL_SEQMUTATE_INST_TRANSPOSE_WINDOW_DISP_PARETO_SCALE_PARAM,
+    float, 2.0f,
+    "Scale param (\"lambda\") for distn of window transposition displacements"
+    " when drawn from pareto distribution. "
+    "Constrained > 0."
   ),
 
   VALUE(ENVIROMENT_STATES, size_t, 4, "TODO"),
