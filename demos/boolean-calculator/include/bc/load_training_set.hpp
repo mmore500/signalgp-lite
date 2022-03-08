56b39f02e1e1f6cee3cbf6d263d4b98928e5e2d7
rodsan0 <rodsan0@users.noreply.github.com>
2022-03-08 14:15:11 -0500

    Cache grouped set properly

--- demos/boolean-calculator/include/bc/load_training_set.hpp
+++ demos/boolean-calculator/include/bc/load_training_set.hpp
@@ -30,19 +30,28 @@ const emp::vector<bc::TestCase>& load_training_set() {
 }
 
 auto& load_grouped_training_set() {
-  thread_local std::unordered_map<bc::PromptEnum, emp::vector<bc::TestCase>> grouped_set;
+  thread_local std::unordered_map<bc::PromptEnum, emp::vector<bc::TestCase>> cached_grouped_set{ [](){
+    const auto& cases = bc::load_training_set(); // vector<TestCase>
 
-  const auto& cases = bc::load_training_set(); // vector<TestCase>
+    std::unordered_map<bc::PromptEnum, emp::vector<bc::TestCase>> res;
 
-  std::for_each(
-    cases.begin(),
-    cases.end(),
-    [](const auto _case){
-      grouped_set[_case.prompts.front().which].push_back(_case);
-    }
-  );
+    std::for_each(
+      cases.begin(),
+      cases.end(),
+      [&res](const auto _case){
+        res[_case.prompts.front().which].push_back(_case);
+      }
+    );
 
-  return grouped_set;
+    emp_assert(
+    res.size() == magic_enum::enum_count<bc::PromptEnum>() - 1, // ignore NUM
+    "Missing operations in test cases.");
+
+    return res;
+  }() };
+
+  return cached_grouped_set;
+
 }
 
 } // namespace bc
