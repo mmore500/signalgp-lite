63f451a3630c19d6510c6466fca096aef4b815d3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-08 01:31:11 -0700

    Move advance_core to own file

--- include/sgpl/algorithm/execute_core.hpp
+++ include/sgpl/algorithm/execute_core.hpp
@@ -2,8 +2,7 @@
 #ifndef SGPL_ALGORITHM_EXECUTE_CORE_HPP_INCLUDE
 #define SGPL_ALGORITHM_EXECUTE_CORE_HPP_INCLUDE
 
-#include <cassert>
-#include <tuple>
+#include <algorithm>
 
 #include "../../../third-party/Empirical/include/emp/base/assert.hpp"
 #include "../../../third-party/Empirical/include/emp/base/macros.hpp"
@@ -11,55 +10,10 @@
 #include "../hardware/Core.hpp"
 #include "../program/Instruction.hpp"
 #include "../program/Program.hpp"
-#include "../utility/ByteEnumeration.hpp"
+
+#include "advance_core.hpp"
 
 namespace sgpl {
-
-// tried a dispatch table, seemed to do about the same or worse
-// https://eli.thegreenplace.net/2012/07/12/computed-goto-for-efficient-dispatch-tables
-template<typename Spec>
-__attribute__ ((hot))
-inline void advance_core(
-  sgpl::Core<Spec>& state,
-  const sgpl::Program<Spec>& program,
-  typename Spec::peripheral_t& peripheral
-) {
-
-  using library_t = typename Spec::library_t;
-
-  emp_assert( program.size() );
-
-  const auto& instruction = program[ state.GetProgramCounter() ];
-
-  emp_assert( instruction.op_code < library_t::GetSize() );
-
-  // can't use emp_assert due to obsucre macro error
-  #define SGPL_CASE_PAYLOAD(N) \
-    case N: \
-      if constexpr (N < library_t::GetSize()) { \
-        using Functor = typename library_t::template Operation<N>; \
-        Functor::template run<Spec>(state, instruction, program, peripheral);\
-      } else { \
-        assert( false && N ); \
-        __builtin_unreachable(); \
-      } \
-    break;
-
-    static_assert( library_t::GetSize() < 256 );
-
-  switch( instruction.op_code ) {
-
-    EMP_WRAP_EACH( SGPL_CASE_PAYLOAD, SGPL_BYTE_ENUMERATION )
-
-    default:
-      emp_assert( false, instruction.op_code );
-      __builtin_unreachable();
-
-  }
-
-  state.AdvanceProgramCounter( program.size() );
-
-};
 
 template<typename Spec>
 size_t execute_core(
@@ -103,19 +57,6 @@ inline size_t execute_core(
   return execute_core(state, program, peripheral, Spec::switch_steps);
 
 };
-
-template<typename Spec>
-inline void advance_core(
-  sgpl::Core<Spec>& state,
-  const sgpl::Program<Spec>& program
-) {
-
-  using peripheral_t = typename Spec::peripheral_t;
-  peripheral_t peripheral;
-
-  advance_core<Spec>(state, program, peripheral);
-
-}
 
 template<typename Spec>
 inline size_t execute_core(
